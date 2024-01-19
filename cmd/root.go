17b5d497d6683aa6f38b363db79af9cce9aa7b32
Felipe Magrassi <felipe.1magrassi@gmail.com>
2024-01-19 17:28:36 -0300

    cli options and stresser initializer

--- cmd/root.go
+++ cmd/root.go
@@ -4,8 +4,10 @@ Copyright © 2024 NAME HERE <felipe.1magrassi@gmail.com>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/felipemagrassi/angry-customer/internal"
 	"github.com/spf13/cobra"
 )
 
@@ -14,6 +16,17 @@ var rootCmd = &cobra.Command{
 	Short: "A simple application that simulates an angry customer",
 	Long: `A simple application that simulates an angry customer making 
 	infinite requests to a server. It is used to test the resilience of a server.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		url, _ := cmd.Flags().GetString("url")
+		requests, _ := cmd.Flags().GetUint64("requests")
+		concurrency, _ := cmd.Flags().GetUint64("concurrency")
+
+		err := internal.RunStresser(url, requests, concurrency)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	},
 }
 
 func Execute() {
@@ -26,6 +39,10 @@ func Execute() {
 func init() {
 	rootCmd.Flags().StringP("url", "u", "", "URL to make requests")
 	rootCmd.Flags().Uint64P("requests", "r", 0, "Number of requests to make")
-	rootCmd.Flags().Uint64P("concurrency", "c", 0, "Number of concurrent requests")
+	rootCmd.Flags().Uint64P("concurrency", "c", 1, "Number of concurrent requests")
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.MarkPersistentFlagRequired("url")
+	rootCmd.MarkPersistentFlagRequired("requests")
+	rootCmd.MarkPersistentFlagRequired("concurrency")
+
 }
--- internal/stresser.go
+++ internal/stresser.go
@@ -1,5 +1,30 @@
 package internal
 
-func Stresser() {
+import (
+	"errors"
+	"net/http"
+)
 
+func RunStresser(url string, numberOfRequests, numberOfThreads uint64) error {
+	if url == "" {
+		return errors.New("url is required")
+	}
+
+	if numberOfRequests < 1 {
+		return errors.New("number of requests must be greater than 0")
+	}
+
+	if numberOfThreads < 1 {
+		return errors.New("number of threads must be greater than 0")
+	}
+
+	stresser(url, numberOfRequests, numberOfThreads)
+	return nil
+}
+
+func stresser(url string, numberOfRequests, numberOfThreads uint64) {
+}
+
+func makeRequest(url string) {
+	http.Get(url)
 }
