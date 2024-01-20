403f8c9889ec5bfa7ebdbe206dc174b35f5fde73
Felipe Magrassi <felipe.1magrassi@gmail.com>
2024-01-20 18:23:11 -0300

    working-initial-version

--- internal/report.go
+++ internal/report.go
@@ -1,4 +1,54 @@
 package internal
 
-func Report() {
+import (
+	"fmt"
+	"time"
+)
+
+type Report struct {
+	totalTime           time.Duration
+	totalRequests       uint64
+	requestErrors       uint64
+	requestDistribution map[int]uint64
+}
+
+func NewReport() *Report {
+	return &Report{
+		requestDistribution: make(map[int]uint64),
+	}
+}
+
+func (r *Report) AddRequest(statusCode int) {
+	r.totalRequests++
+	codeCount, ok := r.requestDistribution[statusCode]
+	if !ok {
+		codeCount = 0
+		r.requestDistribution[statusCode] = codeCount
+	}
+	r.requestDistribution[statusCode] = codeCount + 1
+}
+
+func (r *Report) AddRequestError() {
+	r.totalRequests++
+	r.requestErrors++
+}
+
+func (r *Report) SetTotalTime(totalTime time.Duration) {
+	r.totalTime = totalTime
+}
+
+func (r *Report) Print() {
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Report:")
+	fmt.Println("Total time: ", r.totalTime)
+	fmt.Println("Total requests: ", r.totalRequests)
+	fmt.Println("Successful requests: ", r.totalRequests-r.requestErrors)
+	fmt.Println("Requests with errors: ", r.requestErrors)
+	fmt.Println("200:", r.requestDistribution[200])
+	for code, count := range r.requestDistribution {
+		if code != 200 {
+			fmt.Printf("%d: %d\n", code, count)
+		}
+	}
 }
--- internal/stresser.go
+++ internal/stresser.go
@@ -2,7 +2,14 @@ package internal
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
+	"sync"
+	"time"
+)
+
+var (
+	wg sync.WaitGroup
 )
 
 func RunStresser(url string, numberOfRequests, numberOfThreads uint64) error {
@@ -18,13 +25,52 @@ func RunStresser(url string, numberOfRequests, numberOfThreads uint64) error {
 		return errors.New("number of threads must be greater than 0")
 	}
 
+	if numberOfThreads > numberOfRequests {
+		fmt.Println("Number of threads greater than number of requests, setting number of threads to number of requests")
+		numberOfThreads = numberOfRequests
+	}
+
 	stresser(url, numberOfRequests, numberOfThreads)
 	return nil
 }
 
 func stresser(url string, numberOfRequests, numberOfThreads uint64) {
+	report := NewReport()
+	fmt.Println(report.requestDistribution)
+	fmt.Printf("Starting stresser with %d requests and %d threads\n", numberOfRequests, numberOfThreads)
+
+	threadControl := make(chan struct{}, numberOfThreads)
+	start := time.Now()
+
+	for i := uint64(0); i < numberOfRequests; i++ {
+		threadControl <- struct{}{}
+		printProgressBar(i, numberOfRequests, len(threadControl))
+		wg.Add(1)
+		go makeRequest(url, threadControl, report)
+	}
+
+	wg.Wait()
+	finish := time.Now()
+
+	report.SetTotalTime(finish.Sub(start))
+	report.Print()
 }
 
-func makeRequest(url string) {
-	http.Get(url)
+func makeRequest(url string, threadControl <-chan struct{}, report *Report) {
+	defer wg.Done()
+	res, err := http.Get(url)
+	if err != nil {
+		report.AddRequestError()
+		<-threadControl
+		return
+	}
+
+	time.Sleep(200 * time.Millisecond)
+	go report.AddRequest(res.StatusCode)
+
+	<-threadControl
+}
+
+func printProgressBar(current, total uint64, threadLen int) {
+	fmt.Printf("\r%d%% [%d/%d] Threads: %d", (current*100)/total, current, total, threadLen)
 }
